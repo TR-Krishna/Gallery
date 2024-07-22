62f639f27d2026008e8f39f9a98fbfae4fe2aa00
Keerthana T P <tpkeerthana944@gmail.com>
2024-07-22 23:28:28 +0530

    first commit

---
+++ server.mjs
@@ -0,0 +1,59 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import pg from 'pg';
+import bcrypt from 'bcrypt'; 
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3002;
+
+const db=new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"Ponnu_2133",
+  port:5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+async function check(username, password) {
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      return bcrypt.compare(password, user.password); 
+    }
+    return false;
+  } catch (err) {
+    console.error('Database query error:', err);
+    return false;
+  }
+}
+
+app.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  const isValidUser = await check(username, password);
+
+  if (isValidUser) {
+    res.json({ message: 'Login successful!' });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password!' });
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
